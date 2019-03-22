5bfd25fa119de060ee668affd27c2b8262b3586f
Yinhao Li <liyinhao0413@qq.com>
2019-03-22 21:16:07 +0800

    transform Client and Server to class form

--- Client.py
+++ Client.py
@@ -1,15 +1,56 @@
 import socket
-s= socket.socket()
-host = socket.gethostname()
-port = 12345
-s.connect((host,port))
-print('Linked')
-info = ''
-while info != 'exit':
-  print('SCIENCE:'+info)
-  send_mes=input()
-  s.send(send_mes.encode())
-  if send_mes =='exit':
-    break
-  info = s.recv(1024).decode()
-s.close()
+from DES import DES
+
+
+PORT = 12345
+KEY = "12345678"
+
+
+
+
+class Client():
+    def __init__(self,port):
+        self.port = port
+        self.host = socket.gethostname()
+        self.s = socket.socket()
+        self.s.connect((self.host,self.port))
+        print('Linked')
+        self.des = DES()
+
+
+
+    def communicate(self):
+        info = ""
+        while info != "exit":
+            print('Server:' + info)
+            send_mes = input()
+            send_mes_encrypted = self.des.DES(send_mes,KEY,0)
+            self.s.send(send_mes.encode())
+            if send_mes == "exit":
+                break
+            info_encrypted = self.s.recv(1024).decode()
+            print(info_encrypted)
+            # info = self.des.DES(info_encrypted,KEY,1)
+        self.s.close()
+
+
+
+if __name__ == '__main__':
+    server = Client(PORT)
+    server.communicate()
+
+
+# s= socket.socket()
+# host = socket.gethostname()
+# port = 12345
+# s.connect((host,port))
+# print('Linked')
+# info = ''exit
+# while info != 'exit':
+#   print('SCIENCE:'+info)
+#   send_mes=input()
+#   s.send(send_mes.encode())
+#   if send_mes =='exit':
+#     break
+#   info = s.recv(1024).decode()
+# s.close()
--- Server.py
+++ Server.py
@@ -1,18 +1,62 @@
 import socket
-host = socket.gethostname()
-port = 12345
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(1)
-sock,addr = s.accept()
-print('Connection built')
-info = sock.recv(1024).decode()
-while info != 'exit':
-  print('MOOD:'+info)
-  send_mes = input()
-  sock.send(send_mes.encode())
-  if send_mes =='exit':
-    break
-  info = sock.recv(1024).decode()
-sock.close()
-s.close()
+from DES import DES
+
+
+
+PORT = 12345
+KEY = "12345678"
+
+
+
+class Server():
+    def __init__(self,port):
+        self.host = socket.gethostname()
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.bind((self.host,port))
+        self.s.listen(1)
+        self.sock,self.addr = self.s.accept()
+        self.des = DES()
+        print('Connection built')
+
+
+
+    def communicate(self):
+        info = self.sock.recv(1024).decode()
+        # info = self.des.DES(info_encrypted,KEY,1)
+        while info != 'exit':
+          print('MOOD:'+info)
+          send_mes = input()
+          self.sock.send(send_mes.encode())
+          if send_mes =='exit':
+            break
+          info = self.sock.recv(1024).decode()
+        self.sock.close()
+        self.s.close()
+
+
+
+
+
+if __name__ == '__main__':
+    server = Server(PORT)
+    server.communicate()
+
+
+
+# host = socket.gethostname()
+# port = 12345
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.bind((host,port))
+# s.listen(1)
+# sock,addr = s.accept()
+# print('Connection built')
+# info = sock.recv(1024).decode()
+# while info != 'exit':
+#   print('MOOD:'+info)
+#   send_mes = input()
+#   sock.send(send_mes.encode())
+#   if send_mes =='exit':
+#     break
+#   info = sock.recv(1024).decode()
+# sock.close()
+# s.close()
